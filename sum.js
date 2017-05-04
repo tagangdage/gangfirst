686833afaa4ab903e3a4971a59e30cba5b80d06f
Administrator <zhengzg_flexi@yeah.net>
2017-05-04 14:36:38 +0800

    这是

---
+++ sum.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/5/3.
+ */
+function ab(a,b  )
+{
+	var sum=0;
+	for (var i=a;i<=b;i++)
+	{
+		 sum +=i;
+	}
+	return sum;
+}
+module.exports=ab;
+// console.log(ab( 5,10));
